70f0b5d70445711eefde5009cbb6a6aa71600453
Patrícia Sá <116208299+SaPatricia@users.noreply.github.com>
2025-09-10 21:02:25 -0300

    Create Soma.java

---
+++ exercicio01/Soma.java
@@ -0,0 +1,20 @@
+/**/
+
+import java.util.Scanner;
+
+public class Soma {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int a = sc.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int b = sc.nextInt();
+
+        int soma = a + b;
+        System.out.println("A soma é: " + soma);
+
+        sc.close();
+    }
+}
