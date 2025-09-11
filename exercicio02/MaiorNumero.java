681d44e2fd632d4d3a84553131a741ea87811835
Patrícia Sá <116208299+SaPatricia@users.noreply.github.com>
2025-09-10 22:47:42 -0300

    Create MaiorNumero.java

---
+++ exercicio02/MaiorNumero.java
@@ -0,0 +1,19 @@
+/*3. Construir um programa em Java que leia 2 números inteiros e imprima o maior número lido.*/
+
+import java.util.Scanner;
+
+public class MaiorNumero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o primeiro número: ");
+        int n1 = sc.nextInt();
+        System.out.print("Digite o segundo número: ");
+        int n2 = sc.nextInt();
+
+        if (n1 > n2) {
+            System.out.println("Maior número: " + n1);
+        } else {
+            System.out.println("Maior número: " + n2);
+        }
+    }
+}
