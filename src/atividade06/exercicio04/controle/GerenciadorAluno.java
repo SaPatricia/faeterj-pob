d20ea848bebbafd2919ccedd5b0451714641b770
Patrícia Sá <116208299+ssasummer@users.noreply.github.com>
2025-10-29 20:19:42 -0300

    Create GerenciadorAluno.java

---
+++ src/atividade06/exercicio04/controle/GerenciadorAluno.java
@@ -0,0 +1,25 @@
+package controle;
+
+import dominio.Aluno;
+import java.util.Scanner;
+
+public class GerenciadorAluno {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nome do aluno: ");
+        String nome = scanner.nextLine();
+
+        double nota;
+        do {
+            System.out.print("Nota do aluno (0 a 10): ");
+            nota = scanner.nextDouble();
+        } while (nota < 0 || nota > 10);
+
+        Aluno aluno = new Aluno(nome, nota);
+        System.out.println("Classificação: " + aluno.classificarAluno());
+
+        scanner.close();
+    }
+}
